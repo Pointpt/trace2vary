446e41d74d9c96100563a0601a0038174fd5f1ea
Tassio Vale <tassio.vale@ufrb.edu.br>
2018-02-12 13:07:02 -0300

    Adding training set data.

--- preprocessing/merge_training_data.py
+++ preprocessing/merge_training_data.py
@@ -2,12 +2,16 @@ import glob
 import os
 import pandas as pd
 
-os.chdir("../projects/training")
 
-data = pd.DataFrame()
+def get_training_data_frame():
 
-for file in glob.glob("*.csv"):
-    data_frame = pd.read_csv(file, index_col=0)
-    data = data.append(data_frame)
+    os.chdir("../projects/training")
 
-data.to_csv('full_training_set.csv')
+    data = pd.DataFrame()
+
+    for file in glob.glob("*training_set.csv", recursive=True):
+        data_frame = pd.read_csv(file, index_col=0)
+        data = data.append(data_frame)
+
+    return data
+
